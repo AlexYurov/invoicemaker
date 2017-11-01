208f35c6cac9fcab97224d4550e9bdccad02e57c
Alex Yurov <sasha.yurov@gmail.com>
2017-11-01 12:46:09 +0200

    improved version

--- InvoiceMaker/Constants.cs
+++ InvoiceMaker/Constants.cs
@@ -1,10 +1,21 @@
-﻿namespace InvoiceMaker
+﻿using System;
+
+namespace InvoiceMaker
 {
-    public class Constants
+    public static class Constants
     {
+        public static DateTime ContractStartDate = new DateTime(2016, 6, 1, 0, 0, 0);
+        public const string Description_QualityAssurance = "Quality assurance services";
         public const string Description_Dev = "Software development services";
         public const string Description_Content = "Content management services";
         public const string Description_DataAnalysis = "Data analyst services";
         public const string Description_Design = "Graphic design services";
+
+        public const int ID_AYurov = 55290;
+        public const int ID_BShchudlo = 115639;
+        public const int ID_IPopkov = 95815;
+        public const int ID_ADvoretska = 63497;
+        public const int ID_OGaiduchok = 139623;
+        public const int ID_ShKassal = 220283;
     }
 }
--- InvoiceMaker/MainWindow.xaml.cs
+++ InvoiceMaker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using InvoiceMaker.Helpers;
 using InvoiceMaker.Model;
+using Microsoft.Win32;
 using MyAssays.Data.Xml;
 using MyAssays.ReportXmlConversion;
 
@@ -24,24 +26,29 @@ namespace InvoiceMaker
     {
         private readonly List<BillableUnit> _units;
 
+        //https://app.hubstaff.com/reports/my/time_and_activities?utf8=%E2%9C%93&date=2017-10-01&date_end=2017-10-31&user_id=55290&group_by=date&filters%5Bshow_tasks%5D=1&filters%5Bshow_activity%5D=1&filters%5Bsum_date_ranges%5D=1&filters%5Bshow_notes%5D=1&filters%5Bshow_spent%5D=1&filters%5Bshow_billable%5D=&filters%5Binclude_archived%5D=1
+        //TODO: Open in web
+        //TODO: Chrome anonymous tabs
+
         public MainWindow()
         {
             InitializeComponent();
 
             _units = new List<BillableUnit>
             {
-                new BillableUnit {Id = 55290, Description = Constants.Description_Dev, Price = 34.0},//ayurov
-                new BillableUnit {Id = 115639, Description = Constants.Description_Dev, Price = 17.75, Hours = new TimeSpan(96,59,33 )},//bshchudlo
-                new BillableUnit {Id = 95815, Description = Constants.Description_Dev, Price = 12.0, Hours = new TimeSpan(65,17,39)},//ipopkov
-                new BillableUnit {Id = 63497, Description = Constants.Description_Content, Price = 6.93, Hours = new TimeSpan(66,36,31 )},//advoretska
-                new BillableUnit {Id = 139623, Description = Constants.Description_DataAnalysis, Price = 12.0, Hours = new TimeSpan(97,59,38)},//ogaiduchok
-                //new BillableUnit {Id = 0, Description = Constants.Description_Design, Price = 16.0, Hours = new TimeSpan(12,50,45)},//kvynnytska
+                new BillableUnit {Id = Constants.ID_AYurov, Description = Constants.Description_Dev, Price = 34.0},//ayurov
+                new BillableUnit {Id = Constants.ID_BShchudlo, Description = Constants.Description_Dev, Price = 17.75 },//bshchudlo
+                new BillableUnit {Id = Constants.ID_IPopkov, Description = Constants.Description_Dev, Price = 12.0 },//ipopkov
+                new BillableUnit {Id = Constants.ID_ADvoretska, Description = Constants.Description_Content, Price = 8.32 },//advoretska
+                new BillableUnit {Id = Constants.ID_OGaiduchok, Description = Constants.Description_DataAnalysis, Price = 12.0 },//ogaiduchok
+                new BillableUnit {Id = Constants.ID_ShKassal, Description = Constants.Description_QualityAssurance, Price = 7.53 },//shkassal
+                //new BillableUnit {Id = 0, Description = Constants.Description_Design, Price = 16.0 },//kvynnytska
             };
         }
 
-        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        private async void OnGenerateButtonClick(object sender, RoutedEventArgs e)
         {
-            await ProcessUnits(99266, new DateTime(2017, 7, 1), new DateTime(2017, 8, 1).Subtract(TimeSpan.FromSeconds(1)));
+            await ProcessUnits(99266, _invoiceStartDate, _invoiceStartDate.AddMonths(1).Subtract(TimeSpan.FromSeconds(1)));
             var paymentRows = _units
                 .Select((unit, i) =>
                     new ReportGroupTableRow
@@ -78,6 +85,7 @@ namespace InvoiceMaker
                 FontSizeSpecified = true,
                 Items = new BaseReportElement[]
                 {
+                    new ReportText() {Id = "InvoiceNumberValue", Text = new[] {_invoiceNumber.ToString("00")}},
                     new ReportText() {Id = "DayValue", Text = new[] {DateTime.Today.Day.ToString("00")}},
                     new ReportText() {Id = "MonthValue", Text = new[] {DateTime.Today.Month.ToString("00")}},
                     new ReportText() {Id = "YearValue", Text = new[] {DateTime.Today.Year.ToString("0000")}},
@@ -129,10 +137,33 @@ namespace InvoiceMaker
                     },
                 }
             };
-            var outputFilePath = System.IO.Path.GetFullPath("test.docx");
+            var format = FileSaveFormat.Docx;
+            var ext = XmlConverter.GetExtensionFromSaveFormat(format);
+            var defaultFileName = $"{InvoicePrefixTextBox.Text}_{_invoiceNumber}{ext}";
+            var outputFilePath = System.IO.Path.GetFullPath(defaultFileName);
+            var resultFilePath = outputFilePath;
             XmlConverter.Convert(report, outputFilePath, FileSaveFormat.Docx, ConverterEngineType.Word);
-            MessageBox.Show(this, "Generated!");
-            Process.Start(outputFilePath);
+            var dialog = new SaveFileDialog
+            {
+                InitialDirectory = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "OneDrive", "Contracts"),
+                FileName = defaultFileName,
+                Filter = $"{format} Format|*{ext}"
+            };
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                resultFilePath = dialog.FileName;
+                try
+                {
+                    File.Copy(outputFilePath, resultFilePath, true);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show(e.ToString());
+                    return;
+                }
+            }
+            MessageBox.Show(this, $"The report is generated and saved to {resultFilePath}.", "Invoice Maker", MessageBoxButton.OK, MessageBoxImage.Information);
+            Process.Start(resultFilePath);
         }
 
         private async Task ProcessUnits(int projectId, DateTime startDate, DateTime endDate)
@@ -145,6 +176,26 @@ namespace InvoiceMaker
                 var hours = await HubstaffApi.Instance.GetWorkedHours(unit, projectId, startDate, endDate);
                 unit.Hours = hours;
             }
+        }
+
+        private int _invoiceNumber = 0;
+        private DateTime _invoiceStartDate;
+
+        private void OnInvoiceNumberTextBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (int.TryParse(InvoiceNumberTextBox.Text, out _invoiceNumber))
+            {
+                _invoiceStartDate = Constants.ContractStartDate.AddMonths(_invoiceNumber - 1);
+                if (InvoiceDateTextBlock != null)
+                {
+                    InvoiceDateTextBlock.Text = $"{_invoiceStartDate:dd.MM.yyyy} - {_invoiceStartDate.AddMonths(1).Subtract(TimeSpan.FromDays(1)):dd.MM.yyyy}";
+                }
+            }
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            InvoiceNumberTextBox.Text = 17.ToString();
         }
     }
 }
